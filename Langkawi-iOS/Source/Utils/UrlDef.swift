81f761861f24036fff6970f84f1b28d3f0e55e70
Yuki Matsuo <yuki.matsuo@kinto-technologies.com>
2022-10-12 14:50:13 +0900

    Implement web view

---
+++ Langkawi-iOS/Source/Utils/UrlDef.swift
@@ -0,0 +1,14 @@
+//
+//  UrlDef.swift
+//  Langkawi-iOS
+//
+//  Created by 松尾 勇気（Yuki Matsuo） on 2022/10/12.
+//
+
+import Foundation
+
+class UrlDef {
+    static let serviceRule = "https://www.yahoo.co.jp"
+    
+    static let copyright = "https://kinto-jp.com"
+}
